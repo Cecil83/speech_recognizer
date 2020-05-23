0f5e2fbcbf095040678b593f26c1ca1a7fcca3f8
Cecil83 <47425650+Cecil83@users.noreply.github.com>
2020-05-23 11:44:37 +0200

    Add files via upload

---
+++ Speech Recognizer/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Speech_Recognizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enableButton = new System.Windows.Forms.Button();
+            this.DisableButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelPlayer = new System.Windows.Forms.Label();
+            this.labelPosX = new System.Windows.Forms.Label();
+            this.labelPosY = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelDef = new System.Windows.Forms.Label();
+            this.labelAttaq = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enableButton
+            // 
+            this.enableButton.Location = new System.Drawing.Point(579, 275);
+            this.enableButton.Name = "enableButton";
+            this.enableButton.Size = new System.Drawing.Size(149, 59);
+            this.enableButton.TabIndex = 0;
+            this.enableButton.Text = "Enable Speech";
+            this.enableButton.UseVisualStyleBackColor = true;
+            this.enableButton.Click += new System.EventHandler(this.enableButton_Click);
+            // 
+            // DisableButton
+            // 
+            this.DisableButton.Location = new System.Drawing.Point(579, 355);
+            this.DisableButton.Name = "DisableButton";
+            this.DisableButton.Size = new System.Drawing.Size(149, 59);
+            this.DisableButton.TabIndex = 1;
+            this.DisableButton.Text = "Disable Speech";
+            this.DisableButton.UseVisualStyleBackColor = true;
+            this.DisableButton.Click += new System.EventHandler(this.DisableButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(46, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(698, 204);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "Yo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Joueur :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Position en X :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Position en Y :";
+            // 
+            // labelPlayer
+            // 
+            this.labelPlayer.AutoSize = true;
+            this.labelPlayer.Location = new System.Drawing.Point(221, 297);
+            this.labelPlayer.Name = "labelPlayer";
+            this.labelPlayer.Size = new System.Drawing.Size(0, 13);
+            this.labelPlayer.TabIndex = 6;
+            // 
+            // labelPosX
+            // 
+            this.labelPosX.AutoSize = true;
+            this.labelPosX.Location = new System.Drawing.Point(221, 332);
+            this.labelPosX.Name = "labelPosX";
+            this.labelPosX.Size = new System.Drawing.Size(0, 13);
+            this.labelPosX.TabIndex = 7;
+            // 
+            // labelPosY
+            // 
+            this.labelPosY.AutoSize = true;
+            this.labelPosY.Location = new System.Drawing.Point(221, 363);
+            this.labelPosY.Name = "labelPosY";
+            this.labelPosY.Size = new System.Drawing.Size(0, 13);
+            this.labelPosY.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 391);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Attaquant :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 416);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Défenseur :";
+            // 
+            // labelDef
+            // 
+            this.labelDef.AutoSize = true;
+            this.labelDef.Location = new System.Drawing.Point(221, 416);
+            this.labelDef.Name = "labelDef";
+            this.labelDef.Size = new System.Drawing.Size(0, 13);
+            this.labelDef.TabIndex = 11;
+            // 
+            // labelAttaq
+            // 
+            this.labelAttaq.AutoSize = true;
+            this.labelAttaq.Location = new System.Drawing.Point(221, 391);
+            this.labelAttaq.Name = "labelAttaq";
+            this.labelAttaq.Size = new System.Drawing.Size(0, 13);
+            this.labelAttaq.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAttaq);
+            this.Controls.Add(this.labelDef);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelPosY);
+            this.Controls.Add(this.labelPosX);
+            this.Controls.Add(this.labelPlayer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.DisableButton);
+            this.Controls.Add(this.enableButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Speech Recogniser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enableButton;
+        private System.Windows.Forms.Button DisableButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelPlayer;
+        private System.Windows.Forms.Label labelPosX;
+        private System.Windows.Forms.Label labelPosY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelDef;
+        private System.Windows.Forms.Label labelAttaq;
+    }
+}
+
---
+++ Speech Recognizer/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;//Permet la reconnaissance vocale
+using System.Speech.Synthesis;//Permet de faire parler l'ordinateur
+using System.Media;//Permet de lire des fichiers .wav
+using System.Globalization;//Gère les langues disponibles
+using System.Text.RegularExpressions;
+using MouseKeyboardActivityMonitor.WinApi;
+using MouseKeyboardActivityMonitor;
+
+//Ceci est un début de contrôle vocal rien de bien compliqué pour l'instant
+//Pour commencer à parler à Olivia, vous devez tout d'abord appuyer sur le bouton de gauche 
+//puis une fois l'ordre terminer appuyer sur le bouton de droite
+//Chaque commande doit être d'abord rentrée dans les commandes, elle n'apprend pas encore seul ;)
+//Pour ce qui est de la lecture de musique, le programme ne lis que des .wav
+//Amusez-vous bien en tout cas moi je m'éclate
+
+namespace Speech_Recognizer
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+
+        //Pour les touches
+
+        //private readonly KeyboardHookListener m_KeyboardHookManager;
+
+        SpeechRecognitionEngine recEngine =
+        new SpeechRecognitionEngine(new CultureInfo("fr-FR"));//Permet de comprendre le français
+        //SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        SoundPlayer son = new SoundPlayer("C:\\Users\\antho\\Documents\\Speech Recognizer\\23222.wav");
+
+        
+        Player Player1 = new Player();
+
+        bool flagPlayer = false;
+        bool flagPositionX = false;
+        bool flagPositionY = false;
+        bool flagAttaq = false;
+        bool flagDef = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisableButton.Enabled = false;
+            enableButton.Enabled = true;
+
+            //Pour les touches
+
+            //m_KeyboardHookManager = new KeyboardHookListener(new GlobalHooker());
+            //m_KeyboardHookManager.Enabled = true;
+            //m_KeyboardHookManager.KeyPress += HookManager_KeyPress;
+        }
+
+        //Pour la touche espace
+
+        //private void HookManager_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    if(e.KeyChar == (char)Keys.Space)
+        //    {
+        //        if (a == 0)
+        //        {
+        //            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+        //            a = 1;
+        //        }
+        //    }
+
+        //    else 
+        //    {
+        //        a = 0;
+        //        recEngine.RecognizeAsyncStop();
+        //    }
+        //}
+
+        private void enableButton_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            DisableButton.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string>numberList = new List<string>();
+            
+            for (i = 0; i < 10; i++)
+            {
+                numberList.Add(Convert.ToString(i));
+            }
+
+            Choices commands = new Choices();
+            commands.Add(new string[] { "écrit mon nom", "dit bonjour", "bienvenu", "clear",
+                                        "bonjour olivia", "play music", "stop music", "joueur", "0",
+                                        "1", "2", "3", "4", "sinque", "6", "7", "8", "9", "10", "11",
+                                        "position en x", "position en y", "affiche", "attaque", "defense" });
+                                        // Il reconnait les chiffres           
+
+            //commands.Add(regexList);
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //richTextBox1.Text += e.Result.Text;
+            switch (e.Result.Text)
+            {
+                case "dit bonjour":
+                    synthesizer.SpeakAsync("okay ! ");
+
+                    System.Threading.Thread.Sleep(1000);
+                    MessageBox.Show("Hello Anthony");
+                    break;
+                case "écrit mon nom":
+                    richTextBox1.Text += "\nAnthony";
+                    System.Threading.Thread.Sleep(1000);
+                    synthesizer.SpeakAsync("Quel beau prénom ! ");
+                    break;
+                case "clear":
+                    richTextBox1.Text = "Parlez encore";
+                    break;
+                case "bonjour olivia":
+                    synthesizer.SpeakAsync("Bonjour mon nom est Olivia que puis-je faire pour vous ? ");
+                    //richTextBox1.Text += e.Result.Text;
+                    break;
+                case "play music":
+                    synthesizer.SpeakAsync("okay let's go ! ");
+
+                    System.Threading.Thread.Sleep(1000);
+                    son.Play();
+                    break;
+                case "stop music":
+                    son.Stop();
+                    System.Threading.Thread.Sleep(1000);
+                    synthesizer.SpeakAsync("oh dommage c'était bien ");
+                    break;
+                case "sinque":
+                    if (flagPlayer)
+                        Player1.number = 5;
+                    if (flagPositionX)
+                        Player1.positionx = 5;
+                    if (flagPositionY)
+                        Player1.positiony = 5;
+                    if (flagAttaq)
+                        Player1.attaq = 5;
+                    if (flagDef)
+                        Player1.defense = 5;
+
+                    flagPlayer = false;
+                    flagPositionX = false;
+                    flagPositionY = false;
+                    flagAttaq = false;
+                    flagDef = false;
+
+                    labelPlayer.Text = Convert.ToString(Player1.number);
+                    labelPosX.Text = Convert.ToString(Player1.positionx);
+                    labelPosY.Text = Convert.ToString(Player1.positiony);
+                    labelAttaq.Text = Convert.ToString(Player1.attaq);
+                    labelDef.Text = Convert.ToString(Player1.defense);
+                    break;
+                case "affiche":
+                    labelPlayer.Text = Convert.ToString(Player1.number);
+                    labelPosX.Text = Convert.ToString(Player1.positionx);
+                    labelPosY.Text = Convert.ToString(Player1.positiony);
+                    break;
+                case "joueur":
+                    flagPlayer = true;
+                    break;
+                case "position en x":
+                    flagPositionX = true;
+                    break;
+                case "position en y":
+                    flagPositionY = true;
+                    break;
+                case "attaque":
+                    flagAttaq = true;
+                    break;
+                case "defense":
+                    flagDef = true;
+                    break;
+                default:
+                    richTextBox1.Text += "error";
+                    break;
+            };
+
+            if (int.TryParse(e.Result.Text, out i)) //Permet de savoir si c'est un nombre
+            {
+                if (flagPlayer)
+                    Player1.number = Convert.ToInt32(e.Result.Text);
+                if (flagPositionX)
+                    Player1.positionx = Convert.ToDouble(e.Result.Text);
+                if (flagPositionY)
+                    Player1.positiony = Convert.ToDouble(e.Result.Text);
+                if (flagAttaq)
+                    Player1.attaq = Convert.ToDouble(e.Result.Text);
+                if (flagDef)
+                    Player1.defense = Convert.ToDouble(e.Result.Text);
+
+                flagPlayer = false;
+                flagPositionX = false;
+                flagPositionY = false;
+                flagAttaq = false;
+                flagDef = false;
+
+                labelPlayer.Text = Convert.ToString(Player1.number);
+                labelPosX.Text = Convert.ToString(Player1.positionx);
+                labelPosY.Text = Convert.ToString(Player1.positiony);
+                labelAttaq.Text = Convert.ToString(Player1.attaq);
+                labelDef.Text = Convert.ToString(Player1.defense);
+
+                richTextBox1.Text = e.Result.Text;
+            }
+        }
+
+
+
+        private void DisableButton_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            DisableButton.Enabled = false;
+        }
+
+    }
+}
